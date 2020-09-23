785aa0f43e91af7e13e9b7f7edf694d5f5f37685
giulioverazzo <c1pher1@MatebookXPro.localdomain>
2020-09-23 16:38:31 +0200

    added axios and json-server to notes app

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
 // This code is just for theory purpose.
 
-const App = (props) => {
+const App = () => {
 
   // Lo stato è un proprietà privata di un componente. Sopravvive al re-rendering del componente.
-  const [notes, setNotes] = useState(props.notes)
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState({text:'', isImportant:false})
   const [showAll, setShowAll] = useState(true)
+
+  // Effects are executed immediately after rendering or when certain values have changed (second parameter)
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        // a call to a state-updating function triggers the re-rendering of the component.
+        // this means that the function of component const App = () => {...} gets executed.
+        setNotes(response.data)
+      })
+  }, []) //this empty array is a parameter of useEffect that specifies how ofter the effect is run
+
+  console.log('render', notes.length, 'notes')
 
   const addNote = (event) => {
     //this prevents the default action of submitting a form of HTML5 which causes the page to reload. 
