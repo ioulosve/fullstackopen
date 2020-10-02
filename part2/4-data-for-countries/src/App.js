0b6cce6948e82c04dcc2373ad052c1af17a293c7
giulioverazzo <c1pher1@MatebookXPro.localdomain>
2020-10-02 17:18:29 +0200

    wip on data-for-countries

--- part2/4-data-for-countries/src/App.js
+++ part2/4-data-for-countries/src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function App() {
+const Error = ({message}) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{message}</p>
+    </div>
+  )
+}
+
+const CountriesList = ({countries}) => (
+  <div>
+    {countries.map((country) => 
+      <p key={country.id}>{country.name}</p>
+    )}
+  </div>
+)
+
+const App = () => {
+
+  const [country, setCountry ] = useState('')
+  const [countries, setCountries ] = useState([])
+
+  useEffect(() => {
+    axios
+    .get('https://restcountries.eu/rest/v2/all')
+    .then(response => {
+      setCountries(response.data.map((countryEl) => {return {name: countryEl.name, id: countryEl.alpha2Code}}))
+    })
+  },[])
+
+  const handleInputCountry = (event) => {
+    setCountry(event.target.value)
+    const filteredCountries = countries.filter(c => c.name.toUpperCase().includes(country.toUpperCase()))
+    setCountries(filteredCountries)
+  }
+
+  return (
+    <div>
+      <div>find countries <input value={country} onChange={handleInputCountry}/></div>
+      <div>{ country === '' ? <Error message="Please write something"/> : 
+                              <CountriesList countries={countries}/>}
+      </div>
     </div>
   );
 }
